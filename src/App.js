00cab20b403157dd92f0d7e22380357917b5e6d6
Mon024 <moncedricavila@gmail.com>
2023-05-22 21:39:44 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import About from "./routes/About"
 import Project from "./routes/Project"
 import Contact from "./routes/Contact"
 
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <BrowserRouter basename="/my-app">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="/Project" element={<Project />} />
         <Route path="/Contact" element={<Contact />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 }
 
