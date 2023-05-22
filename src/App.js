053bb75183d5d13117d6c1c105e6d1305f35966d
Mon024 <moncedricavila@gmail.com>
2023-05-22 21:52:57 +0800

    commi

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import About from "./routes/About"
 import Project from "./routes/Project"
 import Contact from "./routes/Contact"
 
-import { Routes, Route } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
+    <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="/Project" element={<Project />} />
         <Route path="/Contact" element={<Contact />} />
       </Routes>
+    </>
   );
 }
 
