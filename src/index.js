dd11849921a422b4f8d68a5d017f3652d966dcf9
Mon024 <moncedricavila@gmail.com>
2023-05-23 10:05:37 +0800

    commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
+import './index.css';
 import App from './App';
 import { BrowserRouter} from "react-router-dom"
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/my-app">
     <App />
   </BrowserRouter>,
   document.getElementById('root')
