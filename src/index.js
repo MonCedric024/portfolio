dbb7546d8c4eaff063d5cb4dbf5fb949237b39a7
Mon024 <moncedricavila@gmail.com>
2023-08-10 09:40:18 +0800

    commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { BrowserRouter} from "react-router-dom"
 
 ReactDOM.render(
-  <BrowserRouter basename="/my-porfolio">
+  <BrowserRouter basename="/porfolio">
     <App />
   </BrowserRouter>,
   document.getElementById('root')
